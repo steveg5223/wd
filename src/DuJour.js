b313b6bebfc6d254d9248b823b9837ddba259800
steve goodwin <stv.goodwin@gmail.com>
2021-03-14 21:46:14 -0700

    implement image thumbnails

--- src/DuJour.js
+++ src/DuJour.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import watchlist from './watch';
+import { ArrowLeftRounded } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -13,6 +14,24 @@ const useStyles = makeStyles((theme) => ({
       padding: theme.spacing(1),
       textAlign: 'left',
       color: theme.palette.text.secondary,
+      overflow: 'auto',
+    },
+    paperWrapper : {
+        display: 'flex',
+        width: '90%',
+        height: '90%',
+        borderBottom: '1px solid black',
+    },
+    paperList : {
+        // width: '50%',
+        marginBlockStart: '.1em',
+        /* display: 'flex', */
+    },
+    paperImageWrapper : {
+        float: 'right',
+    },
+    paperImage : {
+        height: '120px',
     },
   }));
   
@@ -24,14 +43,24 @@ const useStyles = makeStyles((theme) => ({
       return (
         <React.Fragment>
             {watchlist.map((watch) => {
+                const image = `/collection/phpsrc/resize_image.php?path=../img/wd/${watch.watchId}/face&width=150&height=150`;
                 return (
                     <Grid item xs={4}>
                         <Paper className={classes.paper}>
-                            <ul>
+                        <div className={classes.paperWrapper}>
+                            <span className={classes.paperImageWrapper}>
+                                    <a>
+                                        <img className={classes.paperImage} src={image}></img>
+                                    </a>
+                                </span>
+                        </div>
+                        <div className={classes.paperWrapper}>
+                        <ul className={classes.paperList}>
                                 <li>Circa: {watch.circa} {watch.make}: {watch.model}</li>
                                 <li>{watch.description}</li>
                                 <li>Last worn: {watch.last_worn}</li>
-                            </ul>
+                            </ul> 
+                        </div>
                         </Paper>
                     </Grid>
                 );
