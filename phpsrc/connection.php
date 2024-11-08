34aae118a94814ddc39e0355f82b4d6543bde058
Steve Goodwin <sgoodwin@snaplogic.com>
2024-11-07 18:08:14 -0800

    comment out statement that caused myqli error

--- phpsrc/connection.php
+++ phpsrc/connection.php
@@ -4,5 +4,5 @@ $user     = "Steveg5223";
 $password = "65bb352Bzs@w^g";
 $database = "wd_watch";
 // $dblink = mysqli_connect($host, $user, $password, $database);
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli = new mysqli($host, $user, $password , $database );
